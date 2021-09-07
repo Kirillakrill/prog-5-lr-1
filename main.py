9cef4ba5fec0c198549e48088842f6648ad6b23a
Kirillakrill <60612332+Kirillakrill@users.noreply.github.com>
2021-09-07 13:46:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from squareseqdigit import squareSequenceDigit
+
+if __name__ == "__main__":
+
+    print(squareSequenceDigit(1))
---
+++ squareseqdigit.py
@@ -0,0 +1,14 @@
+import math
+
+
+def squareSequenceDigit(n):
+    
+
+
+if __name__ == "__main__":
+    print(squareSequenceDigit(1))
+    print(squareSequenceDigit(2))
+    print(squareSequenceDigit(7))
+    print(squareSequenceDigit(12))
+    print(squareSequenceDigit(17))
+    print(squareSequenceDigit(27))
